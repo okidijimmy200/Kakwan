d5bfc4471088d56cb40cf002fe9fe3dd8891b0f8
okidijimmy200 <okidijimmie@gmail.com>
2020-10-26 13:56:09 +0300

    heroku2

--- server/server.js
+++ server/server.js
@@ -40,7 +40,7 @@ app.listen(config.port, (err) => {
 //serve static files if we are in prod
 if(process.env.NODE_ENV === 'production'){
     //set static folder
-    app.use(express.static('clinet/build'));
+    app.use(express.static('client/build'));
 
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
